9a00538b75abd0cd472a3e29ca35a02d76e9ca19
Konstantin Gredeskoul <kigster@gmail.com>
2015-01-10 04:18:10 -0800

    First version with the spectrum analyzer

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# BoomboxDSP
+# kigaboom! 
+
 Teensy + Audio Shield + TFT+ Infrared / Rotary Knobs = fully customizable audio digital signal processing station, with spectrum analyzer, peak meter, EQ and many more functions available via simple controls.
+
+Here is a quick pic of the prototype :)
+
+<img src="images/prototype.jpg" />
---
+++ SpectrumAnalyzer.h
@@ -0,0 +1,19 @@
+/*
+ * SpectrumAnalyzer.h
+ *
+ *  Created on: Jan 5, 2015
+ *      Author: Konstantin Gredeskoul
+ *        Code: https://github.com/kigster
+ *
+ *  (c) 2014 All rights reserved, MIT License.
+ */
+
+#ifndef SPECTRUMANALYZER_H_
+#define SPECTRUMANALYZER_H_
+
+class SpectrumAnalyzer {
+public:
+    SpectrumAnalyzer();
+};
+
+#endif /* SPECTRUMANALYZER_H_ */
