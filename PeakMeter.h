ec80593f05879cfa985341c235a1d5fdb693892f
Konstantin Gredeskoul <kigster@gmail.com>
2015-01-10 23:03:22 -0800

    Fix bug: update when showing; reformat file

--- PeakMeter.h
+++ PeakMeter.h
@@ -26,27 +26,31 @@ class PeakMeter {
 
 public:
     PeakMeter(AudioAnalyzePeak &left, AudioAnalyzePeak &right) :
-        _peakLeft(left), _peakRight(right) {};
+            _peakLeft(left), _peakRight(right) {
+    };
 
     peakReading_t update() {
-      if(fps > 24) {
-        if (_peakLeft.available() && _peakRight.available()) {
-          fps=0;
-          _peaks.left = _peakLeft.read();
-          _peaks.right = _peakRight.read();
+        if (fps > 24) {
+            if (_peakLeft.available() && _peakRight.available()) {
+                fps = 0;
+                _peaks.left = _peakLeft.read();
+                _peaks.right = _peakRight.read();
+            }
         }
-      }
-      return _peaks;
+        return _peaks;
     }
 #ifdef ENABLE_TFT
     void show(TFTHelper *tftHelper) {
-        tftHelper->drawVerticalBar(35, 60, _peaks.left, 0, 140, 120, 10, colors.peakBar, colors.background);
-        tftHelper->drawVerticalBar(35, 60, _peaks.left, 1, 140, 120, 10, colors.peakBar, colors.background);
+        update();
+        tftHelper->drawVerticalBar(35, 60, _peaks.left, 0, 140, 120, 10,
+                colors.peakBar, colors.background);
+        tftHelper->drawVerticalBar(35, 60, _peaks.left, 1, 140, 120, 10,
+                colors.peakBar, colors.background);
     }
 #endif
 
     void printPeaksToSerial() {
-        uint8_t leftPeak = _peaks.left  * 30.0;
+        uint8_t leftPeak = _peaks.left * 30.0;
         uint8_t rightPeak = _peaks.right * 30.0;
         for (cnt = 0; cnt < 30 - leftPeak; cnt++) {
             Serial.print(" ");
@@ -71,7 +75,6 @@ private:
     AudioAnalyzePeak &_peakLeft;
     AudioAnalyzePeak &_peakRight;
 };
-
 
 #endif /* PEAKMETER_H_ */
 #endif /* ENABLE_PEAKS */
